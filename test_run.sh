2339fecbcd6006a0118b7fcb616a350c1e712215
Nan Hu <nan.hu@ttu.edu>
2025-09-12 14:45:53 -0400

    Update test_run.sh

--- test_run.sh
+++ test_run.sh
@@ -1,19 +1,11 @@
 echo "# The pipeline is recommended to run under job submission systems."
 echo "# Step 0: Pipeline Clone"
-git clone -n  https://github.com/nhu92/Mixed-DNA-Project.git --depth=1
-cd Mixed-DNA-Project/
-git checkout HEAD release/v1.1_underconstruction
-git checkout HEAD release/sample_data
-cd ..
-mkdir MDNA_test
-cd MDNA_test
-cp -r ../Mixed-DNA-Project/release/v1.1_underconstruction/* ./
-cp -r ../Mixed-DNA-Project/release/sample_data/* ./
-rm -rf ../Mixed-DNA-Project/
-
+mkdir SPrOut_test
+git clone https://github.com/nhu92/SPrOut.git
+cd SPrOut_test
 
 # If you have your own sample to test, please ignore the following steps.
-echo "## In-silico Mix Generation"
+echo "## Testing Mixes Generation"
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR762/001/ERR7621631/ERR7621631_1.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR762/001/ERR7621631/ERR7621631_2.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR762/002/ERR7621392/ERR7621392_1.fastq.gz
